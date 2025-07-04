df8c15ff568018a6dfd886c3e6c077278480cd04
Treythomas5 <treythomas5@dcc.edu>
2025-07-04 14:34:27 -0500

    Update poboy.py

--- poboy.py
+++ poboy.py
@@ -1,7 +1,7 @@
 item_menu = {
     print("1." "catfish poboy $14.95"),
-    print("2." "roast beef poboy $14.95"),
-    print("3." "sausge poboy $13.95"),
+    print("2." "roast beef poboy $13.95"),
+    print("3." "sausge poboy $12.95"),
     print("4." "gumbo $4.95"),
 }
 user_order = {
@@ -9,11 +9,12 @@ user_order = {
 }
 user_order = input("What would you like to order? Type the appropriate number of the menu item:")
 if(user_order == "2"):
-    print("roast beef poboy $14.95")
+    print("roast beef poboy $13.95")
 else:
     print("Sorry you can only order one item")
-sales_tax = 0.0945 + 14.95
-final_cost = sales_tax
+sales_tax = 0.0945
+tax = 13.95 * 0.0945
+final_cost = 13.95 + tax
 if(user_order == "2"):
     print(f'final cost is ${final_cost:.2f}')
 
